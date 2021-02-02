892e1c3512bed2f7906bb219150a4c630576cbd3
Sean Walsh <seanvw@gmail.com>
2021-02-02 16:44:30 +0100

    adding all files from learning java
    adding all files from learning java

---
+++ workspace/head_first_b1/src/head_first_b1/hello_java_world.java
@@ -0,0 +1,12 @@
+package head_first_b1;
+
+class hello_java_world {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		for (int i=0; i<10; i++){
+			System.out.println("hi" + i);
+			}
+		}
+	}
+
